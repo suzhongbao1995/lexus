34b440ac31a36304b118d3c882ac4c75670111af
zhongbao <suzhongbao@mokahr.com>
2022-12-05 21:26:38 +0800

    部署测试

--- next.config.js
+++ next.config.js
@@ -1,10 +1,18 @@
 /** @type {import('next').NextConfig} */
+
+console.log('process.env.NODE_ENV===', process.env.NODE_ENV);
+const __PROD__ = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   distDir: 'build',
   images: {
     unoptimized: true,
+  },
+  assetPrefix: __PROD__ ? '/' : '',
+  publicRuntimeConfig: {
+    linkPrefix: __PROD__ ? '/' : '',
   },
 };
 
